ab307539f9aedec3550e0cb5ecc0f36016d6d808
Paz David <pazdvdv@gmail.com>
2026-03-22 16:42:08 +0200

    Refactored fragment file structure

--- app/src/main/java/com/example/conducto2/ui/classes/fragments/LiveFragment.java
+++ app/src/main/java/com/example/conducto2/ui/classes/fragments/LiveFragment.java
@@ -1,4 +1,4 @@
-package com.example.conducto2.ui.classes;
+package com.example.conducto2.ui.classes.fragments;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -11,13 +11,10 @@ import androidx.fragment.app.Fragment;
 
 import com.example.conducto2.R;
 
-/**
- * Fragment that displays upcoming or scheduled lessons for the class.
- */
-public class ScheduledFragment extends Fragment {
+public class LiveFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_scheduled, container, false);
+        return inflater.inflate(R.layout.fragment_live, container, false);
     }
 }
